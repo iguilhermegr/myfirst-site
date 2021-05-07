12feeb634dc6c521130fcce4fc71bbd7c72cf5ad
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-05-06 23:10:04 -0300

    ����zendo rodar o servidor local em express

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express()
 
 app.set('view engine', 'ejs');
-app.use(express.static("public/static"));
+app.use(express.static("static"));
 
 app.get('/', function (req, res){
   res.render("index");
+});
+
+app.get('/models', function (req, res){
+  res.render("models");
+});
+
+app.get('/sac', function (req, res){
+  res.render("sac");
+});
+
+app.get('/404', function (req, res){
+  res.render("404");
 });
 
 app.listen(1511);
